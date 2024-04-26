ef224cb4f8769c5a51f8d127b8401359cb313b21
Lucas Quadros <olucasquadros@gmail.com>
2024-04-26 19:49:16 -0300

    update

--- obj/Debug/net8.0/sistema.AssemblyInfo.cs
+++ obj/Debug/net8.0/sistema.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sistema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d55b116822890dacaaf2cee904eac7fb76b793c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("sistema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sistema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
